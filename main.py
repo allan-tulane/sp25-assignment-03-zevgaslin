f1fa7c0bafdd01c3cba03324a6d12e0ef6e3454d
zevgaslin <64289830+zevgaslin@users.noreply.github.com>
2025-04-08 22:42:45 +0000

    push

--- main.py
+++ main.py
@@ -44,4 +44,46 @@ def fast_MED(S, T, MED={}):
 
 
 def fast_align_MED(S, T, MED=None):
-    pass
+    if MED is None:
+        MED = {}
+    if (S, T) in MED:
+        return MED[(S, T)]
+
+    # Base cases
+    if S == "":
+        alignment = ("-" * len(T), T)
+        MED[(S, T)] = alignment
+        return alignment
+    if T == "":
+        alignment = (S, "-" * len(S))
+        MED[(S, T)] = alignment
+        return alignment
+
+    if S[0] == T[0]:
+        align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+        result = (S[0] + align_S, T[0] + align_T)
+        MED[(S, T)] = result
+        return result
+
+    insert_S, insert_T = fast_align_MED(S, T[1:], MED)
+    insert_result = ("-" + insert_S, T[0] + insert_T)
+    insert_cost = 1 + sum(a != b
+                          for a, b in zip(*insert_result))
+
+    delete_S, delete_T = fast_align_MED(S[1:], T, MED)
+    delete_result = (S[0] + delete_S, "-" + delete_T)
+    delete_cost = 1 + sum(a != b
+                          for a, b in zip(*delete_result))
+
+    sub_S, sub_T = fast_align_MED(S[1:], T[1:], MED)
+    sub_result = (T[0] + sub_S, T[0] + sub_T)
+    sub_cost = (1 if S[0] != T[0] else 0) + sum(
+        a != b for a, b in zip(*sub_result))  
+
+    # Choose the min cost
+    best_result = min([(insert_cost, insert_result),
+                       (delete_cost, delete_result), (sub_cost, sub_result)],
+                      key=lambda x: x[0])[1]
+
+    MED[(S, T)] = best_result
+    return best_result
